d668313b1f4ab1d8500df007d2492d04dd72fc84
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2024-08-09 21:28:59 +0545

    api creation

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express =require('express')
 const app = express()
+app.get("/",(req ,res)=>{
+    console.log(req)
+    res.send("Hello Anupa")
+}
+)
+
+app.get("/about",(req ,res)=>{
+    console.log(req)
+    res.send("Hello About")
+}
+)
 
 app.listen(3000,()=>{
     console.log("Nodejs project started")
