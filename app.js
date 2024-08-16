c09900f1eaed46c63433b1fe561976c54be3f29c
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2024-08-16 21:22:24 +0545

    table created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const express =require('express')
 const connectToDatabase = require('./Database/index')
+const Blog = require('./Model/blogModel')
 
 const app = express()
 app.use(express.json())
@@ -12,7 +13,41 @@ app.get("/",(req ,res)=>{
 }
 )
 
-app.post("/blog",(req ,res)=>{
+// app.post("/blog",(req ,res)=>{
+//     console.log(req.body)
+//     res.status(200).json({
+//         message:"blog api hit successfully"
+//     })
+// }
+// )
+
+app.post("/blog",async (req ,res)=>{
+    console.log(req.body)
+    const {title ,subtitle ,description ,image}=req.body
+    if(!title || !description || !image ||!subtitle){
+        res.status(400).json({
+            message :"please provide title,description,subtitle,image"
+        })
+    }
+    await Blog.create({
+        title:title ,
+        subtitle:subtitle,
+        description:description,
+        image:image
+    })
+    console.log(title ,subtitle ,description ,image)
+    res.status(200).json({
+         message:"blog api hit successfully"
+    })
+  
+    // const description =req.body.description
+    // const title = req.body.title
+    // const subtitle = req.body.subtitle
+    // const image =req.body.image
+
+
+
+
     console.log(req.body)
     res.status(200).json({
         message:"blog api hit successfully"
@@ -26,3 +61,8 @@ app.listen(process.env.PORT,()=>{
 
 // mongodb+srv://baralanupa988:jLOiGK8o2miyfUru@cluster0.tm5kq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
+// const me ={
+//     name:"Anupa",
+//     age:22
+// }
+// req.body.title
